7cc4fbd315668907dcc65087bbffa8dce31e62f2
dennis <dennis34@students.cs.ubc.ca>
2022-05-16 01:34:15 -0700

    first draft

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,27 +1,33 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion("text-davinci-002", {
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
-  });
-  res.status(200).json({ result: completion.data.choices[0].text });
+    const completion = await openai.createCompletion("text-davinci-001", {
+        prompt: generatePrompt(req.body.prompt, req.body.language),
+        temperature: 0.6,
+        max_tokens: 1000,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(prompt, language) {
+    console.log(prompt);
+    console.log(language);
+    if (language == "n/a") {
+        return prompt;
+    } else {
+        return `
+        Prompt: name an animal in English and French
+        Response: English: cat French: dog
+        Prompt: name a country in English and Spanish
+        Response: English: Canada Spanish: Canadá
+        Prompt: generate a poem in English and French
+        Response: English: Beneath the stars, I sit and dream Of a world far away Where the sun always shines And the moon never sets. In this world, There are no worries And everyone is happy. I close my eyes And transport myself To this magical place. French: Sous les étoiles, Je m'assois et je rêve D'un monde lointain Où le soleil brille toujours Et la lune ne se couche jamais. Dans ce monde, Il n'y a pas de soucis Et tout le monde est heureux. Je ferme les yeux Et je me transporte Dans ce lieu magique.
+        Prompt: ${prompt} in English and ${language}:
+        Response:`;
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,107 @@
 import Head from "next/head";
 import { useState } from "react";
+import generate from "./api/generate";
 import styles from "./index.module.css";
 
+let RESULTS = [];
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+    const [promptInput, setPromptInput] = useState("");
+    const [languageInput, setLanguageInput] = useState("n/a");
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ animal: animalInput }),
-    });
-    const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
-  }
+    function renderResults() {
+        return RESULTS.slice(0)
+            .reverse()
+            .map((res, i) => (
+                <div key={i.toString()} className={styles.resultsbox}>
+                    <h2>Prompt</h2>
+                    <span className={styles.prompt}>{res.prompt}</span>
+                    <h2>Response</h2>
+                    <div className={styles.english}>
+                        <span>{generateEnglish(res.ans, res.lang)}</span>
+                    </div>
+                    <br></br>
+                    <div
+                        className={
+                            res.lang != "n/a" ? styles.foreign : styles.hidden
+                        }
+                    >
+                        <span>{generateForeign(res.ans, res.lang)}</span>
+                    </div>
+                </div>
+            ));
+    }
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    function generateEnglish(prompt, lang) {
+        const end = prompt.search(lang);
+        if (end > -1) {
+            return prompt.substring(0, end);
+        }
+        return prompt;
+    }
+    function generateForeign(prompt, lang) {
+        const start = prompt.search(lang);
+        if (start > -1) {
+            return prompt.substring(start, prompt.length);
+        }
+        return "";
+    }
+    function createResult(p, res, lang) {
+        const box = { prompt: p, ans: res, lang: lang };
+        RESULTS.push(box);
+        console.log(RESULTS);
+    }
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
-  );
+    async function onSubmit(event) {
+        event.preventDefault();
+        const lang = languageInput;
+        const response = await fetch("/api/generate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                prompt: promptInput,
+                language: languageInput,
+            }),
+        });
+        const data = await response.json();
+        console.log("done");
+        createResult(promptInput, data.result, lang);
+        setPromptInput("");
+    }
+    return (
+        <div>
+            <Head>
+                <title>OpenAI Quickstart</title>
+                <link rel="icon" href="/earth.png" />
+            </Head>
+
+            <main className={styles.main}>
+                <img src="/earth.png" className={styles.icon} />
+                <h3>Mr. WorldWide AI</h3>
+                <form onSubmit={onSubmit}>
+                    <select
+                        type="text"
+                        name="languages"
+                        id="langs"
+                        onChange={(e) => setLanguageInput(e.target.value)}
+                    >
+                        <option value="n/a">n/a</option>
+                        <option value="French">French</option>
+                        <option value="Chinese">Chinese</option>
+                        <option value="Spanish">Spanish</option>
+                    </select>
+                    <input
+                        type="text"
+                        name="prompt"
+                        placeholder="Enter a prompt"
+                        value={promptInput}
+                        onChange={(e) => setPromptInput(e.target.value)}
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
+                <div className={styles.result}>{renderResults()}</div>
+            </main>
+        </div>
+    );
 }
